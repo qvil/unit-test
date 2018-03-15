6e7e655ace2d57aa293491ca7e030d44b7cbefb8
qvil <tshyeon@seowonintech.co.kr>
2018-03-15 17:28:59 +0900

    matcher test & filename test
    * Not working if end of filename is not test.js

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("one plus one is two", () => {
+  expect(2 + 2).toBe(4);
+});
--- test.js
+++
@@ -1,5 +0,0 @@
-const a = require('.');
-const b = require('.');
-
-console.log(a(1))
-console.log(b)
